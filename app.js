eb4b13ebcdba519dc03648f5b185bdeec97039f7
sunny patel <sunnykpatel.skp@gmail.com>
2024-02-05 16:47:31 -0500

    add routers for auth and jobs

--- app.js
+++ app.js
@@ -5,15 +5,16 @@ const app = express();
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
+const authRouter = require('./routes/auth');
+const jobsRouter = require('./routes/jobs');
 
 //to access data in req body
 app.use(express.json());
 // extra packages
 
 // routes
-app.get('/', (req, res) => {
-  res.send('jobs api');
-});
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/jobs', jobsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
