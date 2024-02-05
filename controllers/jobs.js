f071c225f1f94ffcb84100a4a048ce10de8bf109
sunny patel <sunnykpatel.skp@gmail.com>
2024-02-05 16:32:12 -0500

    add controllers for auth and jobs

---
+++ controllers/jobs.js
@@ -0,0 +1,17 @@
+const getAllJobs = async (req, res) => {
+  res.send('get all Jobs');
+};
+const getJob = async (req, res) => {
+  res.send('get Job');
+};
+const createJob = async (req, res) => {
+  res.send('create Job');
+};
+const updateJob = async (req, res) => {
+  res.send('update Job');
+};
+const deleteJob = async (req, res) => {
+  res.send('delete Job');
+};
+
+module.exports = { getAllJobs, getJob, createJob, updateJob, deleteJob };
