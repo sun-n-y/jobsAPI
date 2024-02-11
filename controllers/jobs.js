a76fe28cdfb4331bde353c084a80b6583583ab14
sunny patel <sunnykpatel.skp@gmail.com>
2024-02-11 12:18:33 -0500

    add create job checkpoint

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,15 +1,27 @@
+const { StatusCodes } = require('http-status-codes');
+const JobModel = require('../models/Job');
+const { BadRequestError, NotFoundError } = require('../errors');
+
 const getAllJobs = async (req, res) => {
   res.send('get all Jobs');
 };
+
 const getJob = async (req, res) => {
   res.send('get Job');
 };
+
 const createJob = async (req, res) => {
-  res.json(req.user);
+  console.log(req.body.createdBy);
+  req.body.createdBy = req.user.userId;
+  console.log(req.body.createdBy);
+  const job = await JobModel.create(req.body);
+  res.status(StatusCodes.CREATED).json({ job });
 };
+
 const updateJob = async (req, res) => {
   res.send('update Job');
 };
+
 const deleteJob = async (req, res) => {
   res.send('delete Job');
 };
