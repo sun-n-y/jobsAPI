aeee7648118e53d245bb214186eaa3ac676c9a5f
sunny patel <sunnykpatel.skp@gmail.com>
2024-02-14 11:31:41 -0500

    add security setup for hosting

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 require('dotenv').config();
 require('express-async-errors');
+//security packages
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
+//security packages
 const express = require('express');
 const app = express();
-// error handler
+//error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 const authRouter = require('./routes/auth');
@@ -10,11 +16,21 @@ const jobsRouter = require('./routes/jobs');
 const connectDB = require('./db/connect');
 const authenticateUser = require('./middleware/authentication');
 
+// security/other packages
+app.set('trust proxy', 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, //15 mins
+    max: 100, //limit each ip to 100 requester per 15mins
+  })
+);
 //to access data in req body
 app.use(express.json());
-// extra packages
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
-// routes
+//routes
 app.use('/api/v1/auth', authRouter);
 //authenticated route, so users only have access to their info
 app.use('/api/v1/jobs', authenticateUser, jobsRouter);
